433f1a041eb074ccf083cc502821f812e3bf3c47
edmond gihozo <34583961+EDMONDGIHOZO@users.noreply.github.com>
2024-05-18 16:58:41 +0200

    fix on product page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,18 +1,20 @@
-import Wrapper from "@/components/wrapper";
+import Wrapper from '@/components/wrapper';
 
+interface Params {
+    id: string;
+}
 
-export default async function TicketDetails({params}) {
+interface TicketDetailsProps {
+    params: Params;
+}
 
+export default async function TicketDetails({ params }: TicketDetailsProps) {
     return (
         <Wrapper>
-            <div className={"grid grid-cols-1 lg:grid-cols-4 space-x-20"}>
-                <div>
-                    product details
-                </div>
-                <div>
-                    ordering panel
-                </div>
+            <div className="grid grid-cols-1 lg:grid-cols-4 space-x-20">
+                <div>product details {params.id}</div>
+                <div>ordering panel</div>
             </div>
         </Wrapper>
-    )
+    );
 }
